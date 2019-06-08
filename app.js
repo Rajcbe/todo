1eba7c1e17d196beebba51fee2cd1bbc6f48398e
apple <pushparajpbe@gmail.com>
2019-06-09 01:56:39 +0530

    search and filter todos

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const form=document.querySelector('.add');
 const ul=document.querySelector('.list-group');
+const search=document.querySelector('.search input');
 const generateTemplate=(todo)=>{
     ul.innerHTML +=`<li class="list-group-item d-flex justify-content-between align-items-center">
         <span>${todo}</span>
@@ -23,4 +24,25 @@ ul.addEventListener('click',(e)=>{
     if(e.target.classList.contains('delete')){
         e.target.parentElement.remove();
     }
+});
+
+//search and filter todolist
+const searchTodo=(searchValue)=>{
+    Array.from(ul.children).filter((list)=>{
+        return !list.textContent.includes(searchValue);
+    }).forEach((list)=>{
+         list.classList.add('filtered');
+    });
+    Array.from(ul.children).filter((list)=>{
+        return list.textContent.includes(searchValue);
+    }).forEach((list)=>{
+        list.classList.remove('filtered');
+    });
+}
+
+
+search.addEventListener('keyup',()=>{
+const searchValue=search.value.trim().toLowerCase();
+searchTodo(searchValue);
+
 });
